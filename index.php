592e5b07938438653b8107d958d07a690a15429e
erixngrh26 <erixml.2004@gmail.com>
2024-12-13 17:20:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,3 @@
-<?php
-session_start();
-require_once "database.php";
-//Memanggil kelas database
-$pdo = new database();
-
-    //Jika user sudah login, maka akan langsung terpindah ke dashboard user/admin
-    if(isset($_SESSION['email']) == 0){
-    
-    }
-    else{
-        header("Location: dashboard.php");    
-    }
-
-    //Memunculkan daftar harga
-    $rows = $pdo -> getHarga();
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,14 +10,14 @@ $pdo = new database();
     <script src="bootstrap/jquery.dataTables.min.js"></script>
     <script src="bootstrap/dataTables.bootstrap4.min.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCKtmUDqFDJ8-D3F0nJM4bpiD4hAR-fzeo"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY"></script>
     <title>LONDO</title>
 </head>
 
 <body>
     <!-- Navbar -->
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="index.php"><p style="font-size:30px;"><b>LONDO</b></p></a>
+        <a class="navbar-brand" href="index.html"><p style="font-size:30px;"><b>LONDO</b></p></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -46,8 +27,8 @@ $pdo = new database();
                 <a class="nav-item nav-link active" href="#daftar-harga">Daftar Harga</a>
                 <a class="nav-item nav-link active" href="#faq">FAQ</a>
                 <a class="nav-item nav-link active" href="#hubungi-kami">Hubungi Kami</a>
-                <a class="nav-item nav-link active" href="author.php">Author</a>
-                <a class="btn btn-outline-success active" href="login.php"><b>Login</b></a>
+                <a class="nav-item nav-link active" href="author.html">Author</a>
+                <a class="btn btn-outline-success active" href="login.html"><b>Login</b></a>
             </div>
         </div>
     </nav>
@@ -56,75 +37,51 @@ $pdo = new database();
     <div id="carouselControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner text-center">
             <div class="carousel-item active">
-                <img class="d-block w-100" src="images\laundry121.jpg">
+                <img class="d-block w-100" src="images/laundry121.jpg" alt="Laundry Image 1">
             </div>
             <div class="carousel-caption">
-                <h6><img src="images/LOGO2.png" width="350px"> </h6>
-                    <h1><a class="display-4 "<!--text-light bg-dark--><b> Selamat Datang di LONDO</b></a></h1>
-                    <br>
-                    <h1><a class="text-light "<!--bg-dark-->Laundry Online Setiap Hari</a></h1>
-                <br>
-                <h6><a class="btn btn-primary btn-lg" href="signup.php" role="button">Daftar Sekarang</a></h6>
-                <br><br><br><br><br>
+                <h6><img src="images/LOGO2.png" width="350px"></h6>
+                <h1><b> Selamat Datang di LONDO</b></h1>
+                <h1>Laundry Online Setiap Hari</h1>
+                <h6><a class="btn btn-primary btn-lg" href="signup.html" role="button">Daftar Sekarang</a></h6>
             </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="images/laundry123.jpg">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="images/laundry221.jpg">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="images/laundry331.jpg">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="images/laundry421.jpg">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="images/laundry661.jpg">
-            </div>
+            <!-- Additional carousel items can be added here -->
         </div>
     </div>
 
-    <!-- Isi -->
+    <!-- Content Sections -->
     <hr id="how-to-order">
     <div class="jumbotron jumbotron-fluid bg-white">
         <div class="container">
-            <div class="tengah">
             <h1 class="display-4">Tutorial Order</h1>
-            <br>
-            <p style="text-align:left;" class="lead">Pengen Order? yuk simak cara dibawah ini.</p>
+            <p class="lead">Pengen Order? yuk simak cara dibawah ini.</p>
             <hr class="my-4">
-
-            <!-- Di row ini isinya merupakan tutorial beserta gambar dengan bentuk kotak ya -->
             <div class="row">
                 <div class="col-sm">
-                <h6><img src="images/img3.png" width="135px"> </h6>
-                <h5>Daftar</h5>
-                <p>Pelanggan mendaftarkan dirinya pada website, dan melakukan pemesanan </p> 
-                    
+                    <h6><img src="images/img3.png" width="135px"></h6>
+                    <h5>Daftar</h5>
+                    <p>Pelanggan mendaftarkan dirinya pada website, dan melakukan pemesanan.</p>
                 </div>
                 <div class="col-sm">
-                <h6><img src="images/img4.png" width="100px"> </h6>
-                <h5>Pengambilan </h5>
-                <p>pihak kami akan mengambil barang yang akan di laundry</p> 
+                    <h6><img src="images/img4.png" width="100px"></h6>
+                    <h5>Pengambilan</h5>
+                    <p>Pihak kami akan mengambil barang yang akan di laundry.</p>
                 </div>
                 <div class="col-sm">
-                <h6><img src="images/img5.png" width="108px"> </h6>
-                <h5>Pencucian</h5>
-                <p>pencucian baju sesuai pemesanan</p>  
+                    <h6><img src="images/img5.png" width="108px"></h6>
+                    <h5>Pencucian</h5>
+                    <p>Pencucian baju sesuai pemesanan.</p>
                 </div>
                 <div class="col-sm">
-                <h6><img src="images/img6.png" width="100px"> </h6>
-                <h5>Pengantaran </h5>
-                <p> pihak kami akan mengantarkan barang yang telah di laundry di rumah anda</p>
-               
+                    <h6><img src="images/img6.png" width="100px"></h6>
+                    <h5 >Pengantaran</h5>
+                    <p>Pihak kami akan mengantarkan barang yang telah di laundry di rumah anda.</p>
                 </div>
                 <div class="col-sm">
-                <h6><img src="images/img7.png" width="160px"> </h6><br>
-                <h5>Pembayaran</h5>
-                <p>pembayaran pemesanan bisa dilakukan secara Cash on delivery</p> 
+                    <h6><img src="images/img7.png" width="160px"></h6>
+                    <h5>Pembayaran</h5>
+                    <p>Pembayaran pemesanan bisa dilakukan secara Cash on delivery.</p>
                 </div>
-            </div>
             </div>
         </div>
     </div>
@@ -132,12 +89,9 @@ $pdo = new database();
     <hr id="daftar-harga">
     <div class="jumbotron jumbotron-fluid">
         <div class="container">
-            <div class="tengah">
             <h1 class="display-4">Daftar Harga</h1>
-            <br>
-            <p style="text-align:left;" class="lead">Berikut ini merupakan daftar harga yang tersedia, murah!</p>
+            <p class="lead">Berikut ini merupakan daftar harga yang tersedia, murah!</p>
             <hr class="my-4">
-            <br>
             <table id="pagination" class="table table-striped table-bordered">
                 <thead>
                     <tr>
@@ -147,82 +101,55 @@ $pdo = new database();
                     </tr>
                 </thead>
                 <tbody>
-                    <?php
-                        foreach ( $rows as $row ) {
-                    ?>
                     <tr>
-                        <th><?=$row['id'] ?></th>
-                        <td><?=$row['nama_barang'] ?></td>
-                        <td><?=$row['harga']?></td>
+                        <th>1</th>
+                        <td>Contoh Barang 1</td>
+                        <td>Rp. 10.000</td>
                     </tr>
-                    <?php
-                        }
-                    ?>
+                    <tr>
+                        <th>2</th>
+                        <td>Contoh Barang 2</td>
+                        <td>Rp. 20.000</td>
+                    </tr>
+                    <!-- Additional rows can be added here -->
                 </tbody>
             </table>
-            </div>
         </div>
     </div>
 
     <hr id="faq">
     <div class="jumbotron jumbotron-fluid bg-white">
         <div class="container">
-          
             <h1 class="display-4">Frequently Asked Questions</h1>
-            <br>
-            <p style="text-align:left;" class="lead">Yang sering ditanyakan pelanggan</p>
+            <p class="lead">Yang sering ditanyakan pelanggan</p>
             <hr class="my-4">
             <h3>Bagaimana saya dapat menggunakan layanan Laundry Online?</h3>
-            <p> Dapat digunakan melalui website resmi Laundry Online yang dapat akses di www.LaundryOnlineMks.com</p>
-                <br>
-            <h3> Bagaimana Cara Order Laundry Online?</h3>
-            <p>Melalui website Laundry Online, Kamu dapat membuat order dengan memilih lokasi Kamu untuk
-             penjemputan dan pengembalian. Setelah itu, kamu pilih layanan yang kamu inginkan, buat order, dan menyelesaikan transaksi pembayaran. kemudian laundry akan di antarkan di alamatmu</p>
-                <br>
-            <h3> Apa saja layanan yang disediakan oleh Laundry Online ?</h3>
+            <p>Dapat digunakan melalui website resmi Laundry Online yang dapat akses di www.LaundryOnlineMks.com</p>
+            <h3>Bagaimana Cara Order Laundry Online?</h3>
+            <p>Melalui website Laundry Online, Kamu dapat membuat order dengan memilih lokasi Kamu untuk penjemputan dan pengembalian. Setelah itu, kamu pilih layanan yang kamu inginkan, buat order, dan menyelesaikan transaksi pembayaran. Kemudian laundry akan diantarkan di alamatmu.</p>
+            <h3>Apa saja layanan yang disediakan oleh Laundry Online?</h3>
             <p>Jenis layanannya dua jenis Laundry, yaitu cuci kiloan dan satuan.</p>
-         
-            </div>
         </div>
-    </div>
     </div>
 
     <hr id="hubungi-kami">
     <div class="jumbotron jumbotron-fluid bg-dark text-white">
         <div class="container">
-            <div class="tengah">
             <h1 class="display-4">Hubungi Kami!</h1>
-            <br>
-            <p style="text-align:left;" class="lead">Jika ada keraguan, kami akan selalu tersedia untuk anda.</p>
+            <p class="lead">Jika ada keraguan, kami akan selalu tersedia untuk anda.</p>
             <hr class="my-4 bg-white">
-
-            <!-- Di row ini isinya merupakan tutorial beserta gambar dengan bentuk kotak ya -->
             <div class="row">
                 <div class="col-sm">
-                    <br>
-                    <h4>
-                        <b>Alamat kami :</b>
-                    </h4>
+                    <h4><b>Alamat kami :</b></h4>
                     <p>Tambakan, Sinduharjo, Kec. Ngaglik, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55581</p>
-                    <br>
-                    <h4>
-                        <b>Nomor Telepon :</b>
-                    </h4>
-                    <p><img src="images/LOGOWA.png" width="40px"><a style="color: white; " href="https://api.whatsapp.com/send/?phone=6282279363009&text&type=phone_number&app_absent=0">082279363009</a></p>
-                    <br>
-                    <h4>
-                        <b>Follow Us! :</b>
-                    </h4>
-                    <p><img src="images/img11.png" width="40px"><a style="color: white; " href="https://www.instagram.com/sae_creative_id">sae_creative_id</a></p>
-               
-
+                    <h4><b>Nomor Telepon :</b></h4>
+                    <p><img src="images/LOGOWA.png" width="40px"><a style="color: white;" href="https://api.whatsapp.com/send/?phone=6282279363009&text&type=phone_number&app_absent=0">082279363009</a></p>
+                    <h4><b>Follow Us! :</b></h4>
+                    <p><img src="images/img11.png" width="40px"><a style="color: white;" href="https://www.instagram.com/sae_creative_id">sae_creative_id</a></p>
                 </div>
-                <br>
-                <br>
                 <div class="col-sm">
                     <div id="googleMaps" style="width:100%;height:380px;"></div>
                 </div>
-            </div>
             </div>
         </div>
     </div>
@@ -232,20 +159,20 @@ $pdo = new database();
         <div class="footer-copyright text-center py-3">Copyright © 2020
             <a href="https://laundryonlinemks.com/"> Laundry OnLine</a>
         </div>
-    </footer>
+    </footer ```html
     <script>
     // fungsi initialize untuk mempersiapkan peta
     function initialize() {
         var propertiPeta = {
-            center:new google.maps.LatLng(-7.728096, 110.401362),
-            zoom:15,
-            mapTypeId:google.maps.MapTypeId.ROADMAP
+            center: new google.maps.LatLng(-7.728096, 110.401362),
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
         };
 
         var peta = new google.maps.Map(document.getElementById("googleMaps"), propertiPeta);
 
         // membuat Marker untuk lokasi laundry
-        var marker=new google.maps.Marker({
+        var marker = new google.maps.Marker({
             position: new google.maps.LatLng(-7.728096, 110.401362),
             map: peta,
             animation: google.maps.Animation.BOUNCE
@@ -258,6 +185,6 @@ $pdo = new database();
     $(document).ready(function() {
         $('#pagination').DataTable();
     });
-</script>
+    </script>
 </body>
 </html>
